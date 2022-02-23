b6dfdc889d5592f2dd39f3a11a995246ced3513c
Dan <en07djc@icloud.com>
2022-02-23 12:35:14 +0000

    Added 5 game loop and added scoring system, as well as final score.

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,8 @@
-
+let pScore = 0;
+let cScore = 0;
 
 const getUserChoice = (userInput) => {
-    userInput = userInput.toLowerCase();
+   userInput = prompt("Rock, Paper or Scissors?").toLowerCase();
     if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
         return userInput
     } else {
@@ -27,33 +28,68 @@ const determineWinner = (userChoice, computerChoice) => {
     };
     if (userChoice === 'rock') {
         if (computerChoice === 'paper') {
+            cScore++;
             return 'Sorry, you lose.'
         } else {
+            pScore++;
             return 'You win!'
         }   
     }
     if (userChoice === 'paper') {
         if (computerChoice === 'rock') {
+            cScore++;
             return 'Sorry, you lose.'
         } else {
+            pScore++;
             return 'You win!'
         }
     }
     if (userChoice === 'scissors') {
         if (computerChoice === 'rock') {
+            pScore++;
             return 'You win!'
         } else {
+            cScore++;
             return 'Sorry, you lose.'
         }
     }
 }
 
-const playGame = () => {
-    const userChoice = getUserChoice('Paper');
-    const computerChoice = getComputerChoice();
-    console.log('You threw: ' + userChoice);
-    console.log('Computer threw: ' + computerChoice);
-    console.log(determineWinner(userChoice, computerChoice));
-};
+const game = () => {
+    const playGame = () => {
+        const userChoice = getUserChoice('Paper');
+        const computerChoice = getComputerChoice();
+        console.log('You threw: ' + userChoice);
+        console.log('Computer threw: ' + computerChoice);
+        console.log(determineWinner(userChoice, computerChoice));
+    };
 
-playGame ();
+    playGame ();
+    
+}
+
+for(let i = 0; i < 5; i++) {
+    console.log(game)
+    console.log('Player Score ' + pScore)
+    console.log('Computer Score ' + cScore)
+
+game ();
+
+console.log('Final Player score ' + pScore)
+console.log('Final Computer Score ' + cScore)
+
+const finalScore = () => {
+    if (pScore > cScore) {
+        console.log('YOU WIN!!!')
+    }
+    if (pScore === cScore) {
+        console.log('DRAW')
+    } else {
+        console.log('You lost :(')
+    }
+
+}
+
+finalScore ();
+
+}
